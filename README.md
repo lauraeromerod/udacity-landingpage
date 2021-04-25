09073f732570f97c0a1df7e95f20a0fcc417749d
lauraeromerod <laurae93@gmail.com>
2021-04-24 22:26:55 -0500

    updated styles and JS based on comments

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
-# Landing Page Project 
+# Landing Page Project
 
-In this project, you will find a landing page displaying four sections which can be accessed by clicking on the dynamically created navigation or by scrolling through the page. 
-When a section is visible or it's in the viewport, a random linear gradient background is added to specify that the section is active, and the navigation link will also have a black background.
-The navigation bar will be hidden when you're not scrolling. 
-A return to top button will appear when scrolling below the hero. 
+In this project, you will find a landing page displaying four sections.
+
+## Features
+
+- Each section can be accessed by clicking on the dynamically created navigation or by scrolling through the page.
+- When a section is visible or it's in the viewport, a random linear gradient background is added to specify that the section is active, and the navigation link will also have a black background.
+- The navigation bar will be hidden when you're not scrolling.
+- When scrolling below the hero, a return to top button will appear.
+
+## Demo
+
+https://lauraeromerod.github.io/udacity-landingpage/
+
+## Feedback
+
+If you have any feedback, please reach out to me at lauraeromerod@gmail.com
+
+## License
+
+MIT License
+
+Copyright (c) [2021] [Elena Romero]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- js/app.js
+++ js/app.js
@@ -1,44 +1,36 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
 //Define Global Variables
-
+let backgroundTimer;
+let isScrolling;
 const nav = document.getElementById("navbar__list");
-const sections = [...document.querySelectorAll("section")];
 const header = document.querySelector(".page__header");
 const scrollToTop = document.querySelector(".back-to-top");
 const hero = document.querySelector(".main__hero");
+const sections = [...document.querySelectorAll("section")];
 const sectionExpand = [...document.querySelectorAll(".section-expand")];
-let backgroundTimer;
-let isScrolling;
 const colors = [
   { startColor: "#e58c8a", endColor: "#eec0c6" },
   { startColor: "#d99ec9", endColor: "#f6f0c4" },
   { startColor: "#e5bdf6", endColor: "#d8dede" },
   { startColor: "#f5e3e6", endColor: "#d9e4f5" },
 ];
-
 //End Global Variables
 
 //Start Helper Functions
-
 //Function to remove class name from elements
 const removeActive = (elements, className) => {
   elements.forEach((element) => {
-    if (element.classList.contains(className))
+    if (element.classList.contains(className)) {
       element.classList.remove(className);
+    }
+  });
+};
+
+//Function to remove class name according to the viewport
+const removeActiveSection = (elements, className) => {
+  elements.forEach((element) => {
+    if (element.classList.contains(className) && !isInViewport(element)) {
+      element.classList.remove(className);
+    }
   });
 };
 
@@ -51,86 +43,62 @@ const changeBackground = (section) => {
 };
 
 //Function to remove background style from elements
-const removeBackground = (elements) => {
+const removeBackground = (elements, className) => {
   elements.forEach((element) => {
-    element.style.background = "";
+    if (!element.classList.contains(className)) {
+      window.clearInterval(backgroundTimer);
+      element.style.background = "";
+    }
   });
 };
 
-//Function to get an element's position
+//Function to get an element's top position
 const isInViewport = (element) => {
   const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+  return rect.top < 250 && rect.top >= -200;
 };
-
 //End Helper Functions
 
 //Begin Main Functions
-
 //Builds menu
 const createNav = () => {
   sections.map((section) => {
     const li = document.createElement("li");
     const title = section.getAttribute("data-nav");
-    li.insertAdjacentHTML(
-      "afterbegin",
-      `<a href="#" class="navLink">${title}</a>`
-    );
+    li.insertAdjacentHTML("afterbegin", `<a class="navLink">${title}</a>`);
     li.addEventListener("click", (e) => {
       e.preventDefault();
-      section.scrollIntoView();
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "start",
+      });
     });
     nav.appendChild(li);
   });
 };
 createNav();
 
-//Adds class 'active' to links when section is active
-const addActiveLink = (links) => {
-  sections.forEach((section, index) => {
-    if (section.classList.contains("active")) {
-      links[index].classList.add("active");
-    }
-  });
-};
-
+//Adds class 'active' to navigation links when section is active
 //Removes class 'active' from sections and links
 //Changes the background of the active section according to the set interval
-const addActive = (entries) => {
-  const navLinks = [...document.querySelectorAll(".navLink")];
-  removeActive(sections, "active");
-  removeActive(navLinks, "active");
-  removeBackground(sections);
-  entries.forEach((entry, index) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("active");
-      backgroundTimer = window.setInterval(
-        () => changeBackground(entry.target),
-        1500
-      );
-    } else {
-      window.clearInterval(backgroundTimer);
-    }
-  });
-  addActiveLink(navLinks);
-};
-
 //Add class 'active' to section when near top of viewport
 const activeSection = () => {
-  const options = {
-    rootMargin: "0px",
-    threshold: [1],
-  };
-  const io = new IntersectionObserver(addActive, options);
-  sections.forEach((section) => io.observe(section));
+  const navLinks = [...document.querySelectorAll(".navLink")];
+  removeActiveSection(sections, "active");
+  removeActive(navLinks, "active");
+  removeBackground(sections, "active");
+  sections.forEach((section, index) => {
+    if (isInViewport(section)) {
+      navLinks[index].classList.add("active");
+      section.classList.add("active");
+      backgroundTimer = window.setInterval(
+        () => changeBackground(section),
+        1500
+      );
+    }
+  });
 };
-activeSection();
 
 //Shows return to top button if header is not in viewport
 const activeScrollTop = () => {
@@ -146,7 +114,7 @@ const hideHeader = () => {
   if (header.classList.contains("hidden")) {
     header.classList.remove("hidden");
   }
-  isScrolling = setTimeout(function () {
+  isScrolling = setTimeout(() => {
     if (window.scrollY > 0) {
       header.classList.add("hidden");
     }
@@ -156,6 +124,7 @@ const hideHeader = () => {
 const scrollEvent = () => {
   window.addEventListener("scroll", (event) => {
     activeScrollTop();
+    activeSection();
     window.clearTimeout(isScrolling);
     hideHeader();
   });
@@ -164,7 +133,8 @@ scrollEvent();
 
 //When button is clicked scrolls to top of page
 const scrollTop = () => {
-  scrollToTop.addEventListener("click", function () {
+  scrollToTop.addEventListener("click", (e) => {
+    e.preventDefault();
     window.scrollTo({
       top: 0,
       left: 0,
@@ -173,21 +143,4 @@ const scrollTop = () => {
   });
 };
 scrollTop();
-
-//This code makes sections collapse but it does not look good
-/*
-sectionExpand.forEach(function (section) {
-  section.addEventListener("click", function () {
-    this.classList.toggle("active");
-    const sectionContent = this.nextElementSibling;
-    if (sectionContent.style.maxHeight) {
-      sectionContent.style.maxHeight = null;
-    } else {
-      sectionContent.style.maxHeight = sectionContent.scrollHeight + "px";
-    }
-    console.log(sectionContent);
-  });
-});
-*/
-
 //End Main Functions
